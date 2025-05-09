66e1814ad6fa6f374aca2f3e95b76a6693057c62
ksingh408 <92683960+ksingh408@users.noreply.github.com>
2025-05-09 14:18:52 +0530

    Update sellerController.js

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -1,3 +1,5 @@
+
+
 const Product = require('../models/productModel');
 
 // Create product
@@ -26,6 +28,7 @@ exports.updateProduct = async (req, res) => {
     );
 
     if (!product) return res.status(404).json({ message: 'Product not found' });
+    console.log(product);
     res.json(product);
   } catch (err) {
     res.status(500).json({ message: 'Error updating product' });
@@ -53,3 +56,60 @@ exports.getSellerProducts = async (req, res) => {
     res.status(500).json({ message: 'Error fetching seller products' });
   }
 };
+
+
+// const Product = require('../models/productModel');
+
+// // Create product
+// exports.createProduct = async (req, res) => {
+//   try {
+//     const { name, price, description, image } = req.body;
+//     const sellerId = req.user.id;
+
+//     console.log(sellerId);
+// console.log(req.user);
+
+//     const product = await Product.create({ name, price, description, image, seller: sellerId });
+//     res.status(201).json(product);
+//   } catch (err) {
+//     res.status(500).json({ message: 'Error creating product' });
+//   }
+// };
+
+// // Update product
+// exports.updateProduct = async (req, res) => {
+//   try {
+//     const product = await Product.findOneAndUpdate(
+//       { _id: req.params.id, seller: req.user.id },
+//       req.body,
+//       { new: true }
+//     );
+
+//     if (!product) return res.status(404).json({ message: 'Product not found' });
+//     res.json(product);
+//   } catch (err) {
+//     res.status(500).json({ message: 'Error updating product' });
+//   }
+// };
+
+// // Delete product
+// exports.deleteProduct = async (req, res) => {
+//   try {
+//     const deleted = await Product.findOneAndDelete({ _id: req.params.id, seller: req.user.id });
+
+//     if (!deleted) return res.status(404).json({ message: 'Product not found' });
+//     res.json({ message: 'Product deleted' });
+//   } catch (err) {
+//     res.status(500).json({ message: 'Error deleting product' });
+//   }
+// };
+
+// // Get seller's products
+// exports.getSellerProducts = async (req, res) => {
+//   try {
+//     const products = await Product.find({ seller: req.user.id });
+//     res.json(products);
+//   } catch (err) {
+//     res.status(500).json({ message: 'Error fetching seller products' });
+//   }
+// };
